825f4490bd31babc85bb1f06bc4d18f386cd406e
atetz <mail@adamtetz.nl>
2023-12-02 14:37:36 +0100

    Catch exception in InputReader helper.

--- src/main/java/helpers/InputReader.java
+++ src/main/java/helpers/InputReader.java
@@ -1,5 +1,6 @@
 package helpers;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -9,9 +10,13 @@ public class InputReader {
     public String filePath;
     public List<String> fileLines;
 
-    public InputReader(String fileName) throws Exception {
-        this.filePath = String.format("%s%s", INPUT_PATH, fileName);
-        this.fileLines = Files.readAllLines(Path.of(filePath));
+    public InputReader(String fileName) {
+        try {
+            this.filePath = String.format("%s%s", INPUT_PATH, fileName);
+            this.fileLines = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public List<String> getLines() {
