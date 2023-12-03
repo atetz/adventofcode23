25b32e255008cd3aea452a8ed5226984bf758079
atetz <mail@adamtetz.nl>
2023-12-03 15:43:40 +0100

    finished day2 part 2. Added part 2 tests.

--- src/main/java/day2/Day2.java
+++ src/main/java/day2/Day2.java
@@ -5,11 +5,11 @@ import utils.InputReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.lang.Integer.parseInt;
 
 public class Day2 {
-
     public static String INPUT_FILE = "02.txt";
 
     public static List<String> getPuzzleInput() {
@@ -41,34 +41,48 @@ public class Day2 {
         return sumOfGameIDs;
     }
 
+    public static Integer calculateCubeSetPower(String cubeSet) {
+        int maxRed = 0;
+        int maxGreen = 0;
+        int maxBlue = 0;
+
+        String[] cubes = cubeSet.split(",\\s|;\\s");
+
+        for (String cube : cubes) {
+            String[] splitCube = cube.split("\\s");
+            int power = parseInt(splitCube[0]);
+            String color = splitCube[1];
+            if (Objects.equals(color, "red") && power > maxRed) {
+                maxRed = power;
+            } else if (Objects.equals(color, "green") && power > maxGreen) {
+                maxGreen = power;
+            } else if (Objects.equals(color, "blue") && power > maxBlue) {
+                maxBlue = power;
+            }
+        }
+
+        return maxRed * maxGreen * maxBlue;
+    }
+
     public static Integer puzzleTwo(List<String> puzzleInput) {
 
+        int sumOfCubePowers = 0;
 
         for (String gameLine : puzzleInput) {
-            int gameID = parseInt(gameLine.replaceAll("(Game\\s|:.+$)", ""));
-            gameLine = gameLine.replaceAll("(Game\\s\\d+:|\\s)", "");
-            String[] allDraws = gameLine.split("[;,]");
-
-
-            for (String allDraw : allDraws) {
-                System.out.println(gameID + allDraw);
-
-            }
-
+            String[] splitGameLine = gameLine.split(":\\s");
+            String cubeSet = splitGameLine[1];
+            sumOfCubePowers = sumOfCubePowers + calculateCubeSetPower(cubeSet);
         }
-        return 1;
+        return sumOfCubePowers;
     }
 
     public static void main(String[] args) {
         List<String> puzzleInput = getPuzzleInput();
 
-
         System.out.println("Part one: " + puzzleOne(puzzleInput));
         System.out.println("Part two: " + puzzleTwo(puzzleInput));
 
-
     }
-
 
 }
 
--- src/test/java/day2/Day2Test.java
+++ src/test/java/day2/Day2Test.java
@@ -5,8 +5,7 @@ import utils.InputReader;
 
 import java.util.List;
 
-import static day2.Day2.puzzleOne;
-import static day2.Day2.validateDraw;
+import static day2.Day2.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day2Test {
@@ -19,8 +18,20 @@ class Day2Test {
     }
 
     @Test
-    void validatePartOne() {
+    void validatePuzzleOne() {
         List<String> sampleOne = new InputReader("02_sample1.txt").getLines();
         assertEquals(puzzleOne(sampleOne), 8);
+    }
+
+    @Test
+    void validateCalculateCubeSetPower() {
+        String cubeSet = "9 green, 3 blue; 12 green, 2 blue; 18 green, 1 blue; 14 green; 2 blue, 9 green, 2 red";
+        assertEquals(calculateCubeSetPower(cubeSet), 108);
+    }
+
+    @Test
+    void validatePuzzleTwo() {
+        List<String> sampleOne = new InputReader("02_sample1.txt").getLines();
+        assertEquals(puzzleTwo(sampleOne), 2286);
     }
 }
